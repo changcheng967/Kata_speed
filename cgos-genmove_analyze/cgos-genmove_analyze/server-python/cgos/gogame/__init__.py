d4f0a1191d6c0ed0df732e95298ba14d524e77e7
Cheng Chang <changcheng967@outlook.com>
2025-02-26 20:51:45 -0500

    remove unneccesay

--- cgos-genmove_analyze/cgos-genmove_analyze/server-python/cgos/gogame/__init__.py
+++
@@ -1,4 +0,0 @@
-from .game import Game, sgf
-from .go import GoGame, KoRule, Rule
-
-__all__ = ["GoGame", "Game", "KoRule", "Rule", "sgf"]
--- cgos-genmove_analyze/cgos-genmove_analyze/server-python/run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PYTHONPATH=$PYTHONPATH:./cgos python3 -m unittest discover tests.gogame
